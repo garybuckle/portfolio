6fe40d2137a64203532d6a8194ec6c76a6d51869
Gary Buckle <buckle.g@mac.com>
2022-03-24 12:18:39 +0000

    page transitions

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".controls");
 const sectBtns = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+// TODO: Fix the call back function
+function pageTransitions() {
+  // select active buttons
+  for (let i = 0; sectBtn.i < length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += "active-btn";
+    });
+  }
+}
+
+pageTransitions();
